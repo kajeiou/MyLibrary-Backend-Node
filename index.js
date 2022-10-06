0068df4e134ea0ac9d8b7cd86b96940c7a73dca9
Nabil <nabilkajeiou2000@gmail.com>
2022-10-06 10:57:35 +0200

    add first route

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const {MongoClient,ServerApiVersion} = require('mongodb');
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
 app.listen(2000, ()=> {
-    console.log("Lancement du serveur...")
+    console.log("Lancement du serveur sur le port 2000...")
 })
 
-
-const uri = "mongodb+srv://nabil:7pouEnMxNUw1ZLDU@cluster0.p4pbfxx.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
@@ -20,11 +20,23 @@ client.connect(err => {
   client.close();
 });
 
+// USER
 app.get("/", (req,res)=> {
     res.send("My Library Backend")
 })
 
-
 app.get("/users/login")
 
 app.get("/users/register")
+
+// Modify user
+app.get("/users/:id", (req,res)=> {
+  let id = req.params.id;
+  res.send("id " + id);
+})
+
+// Delete user
+app.delete("/users/:id",(req,res)=> {
+  let id = req.params.id;
+  res.send("id " + id);
+})
