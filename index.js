ca99127a761af662800869414fb86ac4521e52b8
Nabil <nabilkajeiou2000@gmail.com>
2022-10-06 12:45:13 +0200

    add models folder and User

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const { connect } = require('./db/connect');
-
-
+const { connect, db } = require('./db/connect');
+const mongoose = require('mongoose')
 const app = express();
 
 app.use(cors());
@@ -14,17 +13,11 @@ app.listen(2000, ()=> {
 })
 
 
-
-// CORS
-/*app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin",'*');
-  res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE')
-}) 
-*/
 // USER
 
 connect()
+db()
+
 app.get("/", (req,res)=> {
     res.send("My Library Backend")
 })
@@ -44,8 +37,6 @@ app.delete("/users/:id",(req,res)=> {
   let id = req.params.id;
   res.send("id " + id);
 })
-
-
 
 app.get("/books", (req,res)=> {
   res.send("All books")
