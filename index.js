eba3ed998a0ed9bc67fc77e447c8ff9082507bb1
Nabil <nabilkajeiou2000@gmail.com>
2022-10-06 11:10:29 +0200

    remove .env, node_modules

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ client.connect(err => {
   client.close();
 });
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin",'*');
+  res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader("Acess-Control-Allow-Methods", 'GET, POST, PUT? DELETE')
+}) 
+
 // USER
 app.get("/", (req,res)=> {
     res.send("My Library Backend")
